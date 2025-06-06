cf8af8bbfe801eb5f07ff6002d02cd8690bc1256
Steven Collins <stevencollins@acm.org>
2025-06-06 16:22:53 -0400

    Replace skeleton main.go

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
-// main.go
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/steven-collins-omega/simple-weather-server/weather"
+)
 
 func main() {
-	
-	fmt.Println("Hello, World!")
+	svc := weather.NationalWeatherService{}
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		weather.HandleWeatherRequest(w, r, svc)
+	})
+
+	log.Println("Server running on :8080. Try: http://localhost:8080/weather/40.6782,-73.9442")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
